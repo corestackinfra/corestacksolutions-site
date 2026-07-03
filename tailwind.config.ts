ff69f97a9535db4d2b1cd9ec4d978fa319c00fc1
Mendez252 <c.rmendez@gmail.com>
2026-07-03 15:38:50 -0600

    Revert "feat: orbiting tech stack animation around "corestack" in Hero"
    This reverts commit 36d3ff54dbc71b68cc8af9321dd4e382abb78cc5.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,19 +10,6 @@ export default {
       fontFamily: {
         sans: ['Inter', 'system-ui', 'sans-serif'],
       },
-      keyframes: {
-        orbit: {
-          '0%': {
-            transform: 'rotate(calc(var(--angle) * 1deg)) translateY(calc(var(--radius) * 1px)) rotate(calc(var(--angle) * -1deg))',
-          },
-          '100%': {
-            transform: 'rotate(calc(var(--angle) * 1deg + 360deg)) translateY(calc(var(--radius) * 1px)) rotate(calc((var(--angle) * -1deg) - 360deg))',
-          },
-        },
-      },
-      animation: {
-        orbit: 'orbit calc(var(--duration) * 1s) linear infinite',
-      },
     },
   },
   plugins: [],
